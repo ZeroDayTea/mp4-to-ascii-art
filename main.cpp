de2a0d9a160cbb7888ab199f9e89d624328e1013
Patryk The Confused Developer <patrick.dobranowski@gmail.com>
2022-10-25 03:19:53 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "opencv2/opencv.hpp"
+#include <stdio.h>
+#include <string>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace cv;
+using namespace std::chrono;
+
+string pix2chars() {
+
+}
+
+int main() {
+    VideoCapture cap("bakemonogatari.mp4");
+
+    return 0;
+}
